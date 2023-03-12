e0c53e9acc278b64f0deef632537b0be0e00891d
szwathub <szwathub@gmail.com>
2023-03-12 14:08:40 +0800

    update package for spm

--- Package.swift
+++ Package.swift
@@ -6,24 +6,21 @@ import PackageDescription
 let package = Package(
     name: "ExtrasKit",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ExtrasKit",
-            targets: ["ExtrasKit"])
+        /// Products define the executables and libraries produced by a package,and make them visible
+        /// to other packages.
+        .library(name: "ExtrasKit", targets: ["ExtrasKit"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        /// Dependencies declare other packages that this package depends on.
+        /// .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ExtrasKit",
-            dependencies: [],
-            path: "ExtrasKit")
+        /// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        /// Targets can depend on other targets in this package, and on products in packages which this
+        /// package depends on.
+        .target(name: "ExtrasKit", dependencies: [], path: "ExtrasKit")
     ]
 )
